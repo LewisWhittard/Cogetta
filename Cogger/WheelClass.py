475aa3fa9ca31a83ec7b42031afedd736d36be3a
LewisWhittard <63914200+LewisWhittard@users.noreply.github.com>
2021-03-07 17:53:57 +0000

    Add files via upload

---
+++ Cogger/WheelClass.py
@@ -0,0 +1,124 @@
+import CharacterSet
+from random import *
+import string
+
+class Wheel():
+    def __init__(self):
+        self.CharacterSetClass = CharacterSet.EnglishStandard()
+        self.ImportedWheelKey = []
+        self.GenaratedWheelKey = None
+        self.WheelSize = 93
+        
+    def ImportWheelKey(self,WheelKeyImport):
+        self.ImportedWheelKey = WheelKeyImport.split("-")
+        self.ImportedWheelKey = list((map(int, self.ImportedWheelKey)))
+        
+
+        
+    
+    def GenarateWheelKey(self):
+        WheelKey = ""
+        Counter = 0
+        GenaratedWheelKey = self.CharacterSetClass.inputId.copy()
+        shuffle(GenaratedWheelKey)
+        for i in GenaratedWheelKey:
+            if Counter == 93:
+                WheelKey = WheelKey + str(i)
+            else:
+                WheelKey = WheelKey + str(i) + "-"
+            Counter = Counter + 1
+        print (WheelKey)
+        self.GenaratedWheelKey = WheelKey
+        
+    def EncryptChar(self, originalData):
+        ResultPart0 = self.GetCharId(originalData);
+        ResultPart1 = self.GetRandomId(ResultPart0);
+        ResultPart2 = self.GetFinalChar(ResultPart1);
+        return ResultPart2
+    
+    def GetCharId(self, data):
+        CharecterSet = self.CharacterSetClass.inputValues.copy()
+        Counter = 0
+        Result = None
+        for i in CharecterSet:
+            if (data == i):
+                Result = Counter
+                break
+            else:
+                Counter = Counter + 1
+        print(Result)
+        return Result
+        
+    def GetRandomId(self, data):        
+        Result = None
+        for x in range(data + 1):
+            Result = self.ImportedWheelKey[x]
+        print(Result)
+        return Result
+
+   
+    def GetFinalChar(self, data):        
+        CharecterSet = self.CharacterSetClass.inputValues.copy()
+        Counter = 0
+        Result = None
+        for i in CharecterSet:
+            if (Counter == data):
+                Result = i
+                break
+            else:
+                Counter = Counter + 1
+        print(Result)
+        return Result
+    
+    def DecryptChar(self, originalData):
+        ResultPart0 = self.GetCharId(originalData);
+        ResultPart1 = self.GetRandomIdValue(ResultPart0);
+        ResultPart2 = self.GetFinalChar(ResultPart1);
+        return ResultPart2
+    
+    def GetRandomIdValue(self, data):
+        IDSet = self.ImportedWheelKey.copy()
+        
+        Counter = 0
+        for i in IDSet:
+            if (i == data):
+                Result = Counter
+                break
+            else:
+                Counter = Counter + 1
+        print(Result)
+        return Result
+    
+    def PlusOneToId(self):
+        NewImportedWheelKey = []
+        for i in self.ImportedWheelKey:
+            LoopInt = i
+            if (LoopInt == self.WheelSize):
+                NewImportedWheelKey.append(0);
+        
+            else:
+                ElseInt = i + 1
+                NewImportedWheelKey.append(ElseInt);
+        
+        self.ImportedWheelKey.clear()
+        self.ImportedWheelKey = NewImportedWheelKey.copy()
+        
+        for i in self.ImportedWheelKey:
+            print("New ID", i);
+    
+
+            
+        
+        
+    
+   
+    
+        
+                
+                
+                
+                
+            
+            
+            
+        
---
+++ Cogger/WheelHolderClass.py
@@ -0,0 +1,57 @@
+import WheelClass
+
+class WheelHolder():
+    Wheels = []
+    
+    def GenarateKey(self, Number): 
+        KeyList = []
+        Key = ""
+        for x in range(Number):
+            self.Wheels.append(WheelClass.Wheel());
+        
+        for x in range(Number):
+            self.Wheels[x].GenarateWheelKey()
+        
+        Counter = 0
+        for i in self.Wheels:
+            if Counter == Number - 1:
+                Key = Key + str(i.GenaratedWheelKey)
+                print(i);
+            else:
+                Key = Key + str(i.GenaratedWheelKey) + "+"
+                print(i);
+            Counter = Counter + 1
+        print(Key);
+        
+    def ImportKey(self, ImportedKey):
+        Key = ImportedKey
+        WheelKeyList = Key.split("+")
+        for i in WheelKeyList:
+            self.Wheels.append(WheelClass.Wheel());
+        
+        Counter = 0
+        TempWheelList = []
+        for i in WheelKeyList:
+            self.Wheels[Counter].ImportWheelKey(i)
+            Counter = Counter + 1
+            
+    def EncryptMessage(self, Message):
+      FinalOutput = ""
+      Counter = 0
+      for i in Message:
+          NewValue = self.Wheels[Counter].EncryptChar(i);
+          FinalOutput = FinalOutput + NewValue
+          self.Wheels[Counter].PlusOneToId();
+      print(FinalOutput)    
+
+    def DecryptMessage(self, Message):
+      FinalOutput = ""
+      Counter = 0
+      for i in Message:
+          NewValue = self.Wheels[Counter].DecryptChar(i);
+          FinalOutput = FinalOutput + NewValue
+          self.Wheels[Counter].PlusOneToId();
+      print(FinalOutput)
+        
+
+
