e4f6219228e68f6f3008c9c086a576aecedc2c3c
LewisWhittard <63914200+LewisWhittard@users.noreply.github.com>
2022-10-28 22:45:45 +0100

    Add files via upload

--- Cogger/CharacterSet.py
+++ Cogger/CharacterSet.py
@@ -1,13 +1,13 @@
 class EnglishStandard():
-        inputValues = ['1','2','3','4','5','6','7','8','9','0',
-                'q','w','e','r','t','y','u','i','o','p',
-                'a','s','d','f','g','h','j','k','l',
-                'z','x','c','v','b','n','m',
-                'Q','W','E','R','T','Y','U','I','O','P',
-                'A','S','D','F','G','H','J','K','L',
-                'Z','X','C','V','B','N','M',
-                '|','`','¬','!','"','£','$','%','^','&','*','(',')','-','+',
-                '[','{','}',']',';',':','@','\'','#','~','\\','<',',','.','?','/',' ','_','=','>']
+        
+        versionId = 100
+        
+        if (versionId == 100): 
+            numbers100 = ['0','1','2','3','4','5','6','7','8','9']
+            lowerCase100 = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+            higherCase100 = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+            specialCharacters100 = ['!','"','#','$','%','&',"/","'",'(',')','*','+',',','-','.','\\',':',';','<','=','>','?','@','[',']','^','_','`','{','|','}','~',' ','£']
+            inputValues = numbers100 + lowerCase100 + higherCase100 + specialCharacters100
     
         inputId= []
         Counter = 0
@@ -17,19 +17,7 @@ class EnglishStandard():
         maxId = inputId[-1]
         count = len(inputId)
         minId = inputId[0]
-        
-        
-        
-            
-            
-                
-                
-        
-class DemoSet():
-        inputValues = ['A','S','D','F']
-        inputId=[0,1,2,3]
-        maxId = inputId[-1]
-        
+
 class KeySet():
     inputValues = ['1','2','3','4','5','6','7','8','9','0',
                 '+','-']
