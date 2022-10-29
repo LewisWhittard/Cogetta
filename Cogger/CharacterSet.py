adf29aa12cfd5972b4ab828e19aa8bb98ce31200
LewisWhittard <63914200+LewisWhittard@users.noreply.github.com>
2022-10-29 18:01:11 +0100

    Add files via upload

--- Cogger/CharacterSet.py
+++ Cogger/CharacterSet.py
@@ -1,7 +1,9 @@
 class EnglishStandard():
         
-        versionId = 100
+        versionId = "Test Set"
         
+        if (versionId == "Test Set"): 
+            inputValues = ['A','S','D','F']       
         if (versionId == 100): 
             numbers100 = ['0','1','2','3','4','5','6','7','8','9']
             lowerCase100 = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
