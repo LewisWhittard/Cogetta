241b1fd8d140b9912679d643698a1db79d0588a8
LewisWhittard <63914200+LewisWhittard@users.noreply.github.com>
2022-11-06 11:00:44 +0000

    Add files via upload

--- Cogetta/CharacterSet.py
+++ Cogetta/CharacterSet.py
@@ -21,5 +21,4 @@ class EnglishStandard():
         minId = inputId[0]
 
 class KeySet():
-    inputValues = ['1','2','3','4','5','6','7','8','9','0',
-                '+','-']
+    inputValues = ['1','2','3','4','5','6','7','8','9','0','+','-']
