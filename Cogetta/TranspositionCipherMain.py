8ab1e10d9e3f12b2f2ecc3af7c49851257cbf735
LewisWhittard <63914200+LewisWhittard@users.noreply.github.com>
2022-11-18 23:58:39 +0000

    Add files via upload

---
+++ Cogetta/TranspositionCipherMain.py
@@ -0,0 +1,34 @@
+import WheelHolderClass
+import Validation
+
+StringValidation = Validation.StringValidation()
+KeyValidation = Validation.KeyValidation()
+NumberValidation = Validation.NumberValidation()
+
+print("Cogetta Transposition Cipher • © Lewis Whittard 2022 • MIT License • Version: 1.0.0")
+
+ProgramActive = True
+
+while ProgramActive == True:
+    print("0: Genarate a key")
+    print("1: Encrypt")
+    print("2: Decrypt")
+    print("3: Quit")
+
+    Action = input("Please enter a Number:")
+
+    if (Action == "0"):
+        
+   
+    elif (Action == "1"):
+        
+
+    elif (Action == "2"):
+        
+    
+    elif (Action == "3"):
+        ProgramActive = False
+    
+    elif(Action != "0" or Action != "1" or Action != "2"or Action != "3"):
+        print("Incorrect value please refer to the list of commands")
+    
