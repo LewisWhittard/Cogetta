dd1b214e81c8db5cb0113788f04d5f64c49d5e06
LewisWhittard <63914200+LewisWhittard@users.noreply.github.com>
2022-11-18 23:54:04 +0000

    Add files via upload

---
+++ Cogetta/TranspositionCipherColumn.py
@@ -0,0 +1,14 @@
+class Column():
+    def __init__(self, iD):
+        ID = iD
+        ListOfLetters = []
+        FinalString = None
+        
+    def AddLetter(Letter):
+        ListOfLetters.append(Letter)
+        
+    def ReturnListOfLetters():
+        StringToReturn = None
+        for i in List:
+            StringToReturn = StringToReturn + i
+        return StringToReturn
